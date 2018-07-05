37632e4ab0de33678f0eb91f163d7e017bdc639d
Aaron Bauman <aaron.bauman@7signal.com>
2018-07-05 16:06:12 -0400

    Makes sure that build folders exist before creating build artifacts

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+BUILD_DIR="build"
+BUILD_LIB_DIR="build/lib"
+BUILD_NATIVE_DIR="$BUILD_LIB_DIR/native"
+
+function initBuildArtifactFolder() {
+  mkdir -p $BUILD_DIR
+  mkdir -p $BUILD_LIB_DIR
+  mkdir -p $BUILD_NATIVE_DIR
+}
+
 function buildNativeLibrary() {
-  g++ -I/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/ -Isrc/cpp/include -c src/cpp/HelloWorld.cpp -o build/lib/native/HelloWorld.o
-  g++ -dynamiclib -o build/lib/libhelloworld.jnilib build/lib/native/HelloWorld.o
+  g++ -I/System/Library/Frameworks/JavaVM.framework/Versions/Current/Headers/ -Isrc/cpp/include -c src/cpp/HelloWorld.cpp -o $BUILD_NATIVE_DIR/HelloWorld.o
+  g++ -dynamiclib -o build/lib/libhelloworld.jnilib $BUILD_NATIVE_DIR/HelloWorld.o
 }
 
 function buildJniLibrary() {
@@ -10,14 +20,15 @@ function buildJniLibrary() {
 }
 
 function packageLibraryIntoJar() {
-  cp build/lib/libhelloworld.jnilib build/lib/com/sevensignal/helloworld
-  jar -cf build/helloworld.jar -C build/lib com/sevensignal/helloworld
+  cp $BUILD_LIB_DIR/libhelloworld.jnilib $BUILD_LIB_DIR/com/sevensignal/helloworld
+  jar -cf $BUILD_DIR/helloworld.jar -C $BUILD_LIB_DIR com/sevensignal/helloworld
 }
 
 function buildFunctionalTest() {
-  javac -cp build/helloworld.jar:. -d build functional-test/java/com/sevensignal/mosfunctionaltester/TestJni.java
+  javac -cp $BUILD_DIR/helloworld.jar:. -d $BUILD_DIR functional-test/java/com/sevensignal/mosfunctionaltester/TestJni.java
 }
 
+initBuildArtifactFolder
 buildNativeLibrary
 buildJniLibrary
 packageLibraryIntoJar
